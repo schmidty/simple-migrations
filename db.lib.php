d58a0a4a6d904a3e060845577704232b1a022b89
Matthew Schmidt <schmidty99203@gmail.com>
2015-01-08 21:16:04 -0800

    Added config file and check if not set

---
+++ db.lib.php
@@ -0,0 +1,4 @@
+<?php
+$connection_string = 'mysql:host=localhost;dbname=checkbook';
+$db_user = '';
+$db_password = '';
--- migration.php
+++ migration.php
@@ -2,5 +2,8 @@
 require_once 'db.lib.php';
 require_once 'migration.class.php';
 
+if (empty($db_user) || empty($db_password))
+	die("ERROR! You need to set the db.lib.php configs before proceeding...\n");
+
 new Migration($connection_string, $db_user, $db_password);
 
